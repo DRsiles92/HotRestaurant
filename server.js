527424680c1b8f316f1cfaf6b3dfa3405221dda7
Leah Barnes <johnson.leahe@gmail.com>
2020-01-04 11:37:59 -0500

    fix typo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
-appuse(express.json());
+app.use(express.json());
 
 app.listen(PORT, function(){
     console.log("App listening on PORT" + PORT);
